6a1a9c77c220b98da5f3a57b7908fa5873e4dca8
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-11-28 16:37:01 +0100

    Fix problème sonarlint

--- src/main/java/fr/insalyon/Main.java
+++ src/main/java/fr/insalyon/Main.java
@@ -14,50 +14,91 @@ import javafx.stage.Stage;
 
 import static java.lang.Float.max;
 
+import java.util.Optional;
+
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        /* Loading map data */
-        CityMap map = XMLParser.parseFile("data/xml/mediumMap.xml");
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		/* Loading map data */
+		CityMap map = XMLParser.parseFile("data/xml/mediumMap.xml");
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
-        Scene scene = new Scene(root, 1000, 700);
-        Canvas canvas = (Canvas) scene.lookup("#canvas_map");
-        fillMap(map, canvas);
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
+		Scene scene = new Scene(root, 1000, 700);
+		Canvas canvas = (Canvas) scene.lookup("#canvas_map");
+		fillMap(map, canvas);
 
-        primaryStage.setTitle("Calculateur de tours de livraison en vélo");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+		primaryStage.setTitle("Calculateur de tours de livraison en vélo");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
-    private void fillMap(CityMap map, Canvas canvas) {
-        // Getting max lat and long coordinates
-        float maxLat = map.getIntersections().stream().map(Intersection::getLatitude).max(Float::compare).get();
-        float maxLong = map.getIntersections().stream().map(Intersection::getLongitude).max(Float::compare).get();
-        float minLat = map.getIntersections().stream().map(Intersection::getLatitude).min(Float::compare).get();
-        float minLong = map.getIntersections().stream().map(Intersection::getLongitude).min(Float::compare).get();
-        float latDiff = maxLat - minLat;
-        float longDiff = maxLong - minLong;
-        float coeff = max(latDiff, longDiff);
-        // Scaling Graphic Context
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.scale(1, -1);
-        gc.translate(0, -(latDiff * (float)(canvas.getHeight()) / coeff));
-        // Scaling coordinates
-        map.getIntersections().forEach(intersection -> {
-            intersection.setLatitude((intersection.getLatitude() - minLat) * (float)(canvas.getHeight()) / coeff);
-            intersection.setLongitude((intersection.getLongitude() - minLong) * (float)(canvas.getWidth()) / coeff);
-        });
-        map.getIntersections().forEach(intersection -> intersection.getOutwardSegments().forEach(segment -> drawSegment(gc, segment)));
-    }
+	private <T> T getOptionalValue(Optional<T> value) {
+		if (value.isEmpty()) {
+			throw new IllegalArgumentException("No value found");
+		}
+		return value.get();
+	}
 
-    private void drawSegment(GraphicsContext gc, Segment segment) {
-        gc.setStroke(Color.BLUE);
-        gc.strokeLine(segment.getOrigin().getLongitude(), segment.getOrigin().getLatitude(), segment.getDestination().getLongitude(), segment.getDestination().getLatitude());
-    }
+	private float getMaxLatitude(CityMap map) {
+		Optional<Float> max = map.getIntersections()
+				                 .stream()
+								 .map(Intersection::getLatitude)
+								 .max(Float::compare);
+		return getOptionalValue(max);
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	private float getMinLatitude(CityMap map) {
+		Optional<Float> max = map.getIntersections()
+				                 .stream()
+								 .map(Intersection::getLatitude)
+								 .min(Float::compare);
+		return getOptionalValue(max);
+	}
+
+	private float getMaxLongitude(CityMap map) {
+		Optional<Float> max = map.getIntersections()
+				                 .stream()
+								 .map(Intersection::getLongitude)
+								 .max(Float::compare);
+		return getOptionalValue(max);
+	}
+
+	private float getMinLongitude(CityMap map) {
+		Optional<Float> max = map.getIntersections()
+				                 .stream()
+								 .map(Intersection::getLongitude)
+								 .min(Float::compare);
+		return getOptionalValue(max);
+	}
+
+	private void fillMap(CityMap map, Canvas canvas) {
+		// Getting max lat and long coordinates
+		float maxLat = getMaxLatitude(map);
+		float maxLong = getMaxLongitude(map);
+		float minLat = getMinLatitude(map);
+		float minLong = getMinLongitude(map);
+		float latDiff = maxLat - minLat;
+		float longDiff = maxLong - minLong;
+		float coeff = max(latDiff, longDiff);
+		// Scaling Graphic Context
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.scale(1, -1);
+		gc.translate(0, -(latDiff * (float)(canvas.getHeight()) / coeff));
+		// Scaling coordinates
+		map.getIntersections().forEach(intersection -> {
+			intersection.setLatitude((intersection.getLatitude() - minLat) * (float)(canvas.getHeight()) / coeff);
+			intersection.setLongitude((intersection.getLongitude() - minLong) * (float)(canvas.getWidth()) / coeff);
+		});
+		map.getIntersections().forEach(intersection -> intersection.getOutwardSegments().forEach(segment -> drawSegment(gc, segment)));
+	}
+
+	private void drawSegment(GraphicsContext gc, Segment segment) {
+		gc.setStroke(Color.BLUE);
+		gc.strokeLine(segment.getOrigin().getLongitude(), segment.getOrigin().getLatitude(), segment.getDestination().getLongitude(), segment.getDestination().getLatitude());
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
