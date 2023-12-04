10e1610214dca473ad3165135ce2df193c1bd331
arosard <alexandre.rosard@gmail.com>
2023-12-04 16:37:01 +0100

    AddCourierCommand completed

--- src/main/java/fr/insalyon/controller/command/AddCourierCommand.java
+++ src/main/java/fr/insalyon/controller/command/AddCourierCommand.java
@@ -1,8 +1,33 @@
 package fr.insalyon.controller.command;
 
+import fr.insalyon.model.Courier;
+import fr.insalyon.model.DataModel;
+
 /**
  *
  */
-public class AddCourierCommand {
+public class AddCourierCommand implements Command {
+    DataModel dataModel = null;
+    Courier courierToAdd = null;
 
+    /**
+     * Used when initializing the command
+     * @param dm the dataModel where the information is stored
+     * @param c the courier to add
+     */
+    public void addCourier(DataModel dm, Courier c) {
+        dataModel = dm;
+        courierToAdd = c;
+    }
+
+    @Override
+    public void doCommand() {
+        if (!dataModel.getCouriers().contains(courierToAdd))
+            dataModel.getCouriers().add(courierToAdd);
+    }
+
+    @Override
+    public void undoCommand() {
+        dataModel.getCouriers().remove(courierToAdd);
+    }
 }
