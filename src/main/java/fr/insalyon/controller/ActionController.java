38419bc49600e1ad480f7e16e02cf86684524f5c
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-06 11:28:43 +0100

    Ajout des boutons pour les couriers et les delivery sur l'interface

--- src/main/java/fr/insalyon/controller/ActionController.java
+++ src/main/java/fr/insalyon/controller/ActionController.java
@@ -2,24 +2,46 @@ package fr.insalyon.controller;
 
 import fr.insalyon.controller.command.CommandList;
 import fr.insalyon.model.DataModel;
+import javafx.fxml.FXML;
 
 public class ActionController {
+
+	private DataModel dataModel;
+
+	private MainController parentController;
+
+	private CommandList commandList;
+
+	/**
+	 * Initialize the controller variables
+	 * 
+	 * @param dataModel      the controller data
+	 * @param mainController the parent controller
+	 * @param commandList    the command history
+	 */
+	public void initialize(DataModel dataModel, MainController mainController, CommandList commandList) {
+		this.parentController = mainController;
+		this.dataModel = dataModel;
+		this.commandList = commandList;
+	}
+
+	@FXML
+	private void addCourier() {
+		System.out.println("nouveau courier");
+	}
+
+	@FXML
+	private void removeSelectedCourier() {
+		System.out.println("suppression courier");
+	}
+
+	@FXML
+	private void addDelivery() {
+		System.out.println("ajout delivery");
+	}
 	
-    private DataModel dataModel;
-
-    private MainController parentController;
-
-    private CommandList commandList;
-
-    /**
-     * Initialize the controller variables
-     * @param dataModel the controller data
-     * @param mainController the parent controller
-     * @param commandList the command history
-     */
-    public void initialize(DataModel dataModel, MainController mainController, CommandList commandList) {
-        this.parentController = mainController;
-        this.dataModel = dataModel;
-        this.commandList = commandList;
-    }
+	@FXML
+	private void removeSelectedDelivery() {
+		System.out.println("suppression delivery");
+	}
 }
