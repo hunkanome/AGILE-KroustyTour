b24365e4f111be8bc017e1eb32b6928f9a621027
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-06 18:40:23 +0100

    Modification implémentation TimeWindow

--- src/main/java/fr/insalyon/model/TimeWindow.java
+++ src/main/java/fr/insalyon/model/TimeWindow.java
@@ -1,15 +1,14 @@
 package fr.insalyon.model;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
- * A one-hour time interval during which a courier can make a delivery
+ * A one-hour time interval during which a courier can make a delivery.<br/>
+ * It uses a multiton pattern to create all the instances and deliver it
  * 
  * @see Delivery
  * @see Courier
@@ -17,27 +16,38 @@ import javafx.collections.ObservableList;
 public class TimeWindow {
 
 	private static final Duration DURATION = Duration.ofHours(1);
-	
+
 	private static ObservableList<TimeWindow> timeWindows;
 
-	private static final int[] possibleTimeWindows = { 8, 9, 10, 11 };
+	/**
+	 * The start hour of the first TimeWindow
+	 */
+	public static final int MIN_TIME_WINDOW = 8;
+	/**
+	 * The start hour of the last TimeWindow
+	 */
+	public static final int MAX_TIME_WINDOW = 14;
 
 	/**
 	 * @return the list of the available time windows
 	 */
 	public static ObservableList<TimeWindow> getTimeWindows() {
 		if (timeWindows == null) {
-			System.out.println("création time window");
 			timeWindows = FXCollections.observableArrayList();
-			for (int startHour : possibleTimeWindows) {
-				TimeWindow window = new TimeWindow(startHour);
+			for (int i = MIN_TIME_WINDOW; i <= MAX_TIME_WINDOW; i++) {
+				TimeWindow window = new TimeWindow(i);
 				timeWindows.add(window);
 			}
 		}
 		return timeWindows;
 	}
-	
-	public static TimeWindow getTimeWindow(int startHour) {
+
+	/**
+	 * @param startHour - the hour the TimeWindow starts
+	 * @return the TimeWindow instance with the corresponding start hour
+	 * @throws IllegalArgumentException if the start hour is invalid
+	 */
+	public static TimeWindow getTimeWindow(int startHour) throws IllegalArgumentException {
 		for (TimeWindow window : getTimeWindows()) {
 			if (window.getStartHour() == startHour) {
 				return window;
@@ -49,13 +59,7 @@ public class TimeWindow {
 	private final int startHour;
 
 	private TimeWindow(int hour) {
-		for (int tw : possibleTimeWindows) {
-			if (tw == hour) {
-				this.startHour = hour;
-				return;
-			}
-		}
-		throw new IllegalArgumentException("Invalid hour for time window");
+		this.startHour = hour;
 	}
 
 	public int getStartHour() {
@@ -96,6 +100,5 @@ public class TimeWindow {
 		int endHour = this.startHour + (int) DURATION.toHours();
 		return "From " + this.startHour + "h to " + endHour + "h";
 	}
-	
-	
+
 }
