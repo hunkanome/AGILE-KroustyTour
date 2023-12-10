6e5ca737e48d05d47faa168599825b60955924f6
arosard <alexandre.rosard@gmail.com>
2023-12-10 13:25:24 +0100

    Adding forgotten class

---
+++ src/main/java/fr/insalyon/controller/command/RemoveSelectedDeliveryCommand.java
@@ -0,0 +1,31 @@
+package fr.insalyon.controller.command;
+
+import fr.insalyon.algorithm.AStar;
+import fr.insalyon.model.DataModel;
+import fr.insalyon.model.Delivery;
+import fr.insalyon.model.Tour;
+
+public class RemoveSelectedDeliveryCommand implements Command {
+
+    protected DataModel dataModel;
+
+    protected Tour tour;
+
+    protected Delivery delivery;
+
+    public RemoveSelectedDeliveryCommand(DataModel dataModel) {
+        this.dataModel = dataModel;
+        this.tour = dataModel.getSelectedTour();
+        this.delivery = dataModel.getSelectedDelivery();
+    }
+
+    @Override
+    public void doCommand() {
+        this.tour.removeDelivery(this.delivery, this.dataModel.getCityMap(), new AStar());
+    }
+
+    @Override
+    public void undoCommand() {
+        this.tour.addDelivery(this.delivery, dataModel.getCityMap(), new AStar());
+    }
+}
