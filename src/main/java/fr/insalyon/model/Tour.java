457a82b8590ebedcc05c7308d60b5202e9c8f763
arosard <alexandre.rosard@gmail.com>
2023-12-11 10:03:25 +0100

    WIP

--- src/main/java/fr/insalyon/model/Tour.java
+++ src/main/java/fr/insalyon/model/Tour.java
@@ -7,10 +7,7 @@ import fr.insalyon.algorithm.TSP1;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * A sequence of deliveries located on a single path and handled by a courier.
@@ -51,15 +48,18 @@ public class Tour {
     }
 
     private void recalculateGraph(CityMap cityMap, ShortestPathAlgorithm shortestPathAlgorithm){
-        Graph graph = new DeliveryGraph(deliveriesList, cityMap, shortestPathAlgorithm);
+        List<Delivery> deliveries = new ArrayList<>(this.deliveriesList);
+        Delivery warehouseDelivery = new Delivery(this.courier, cityMap.getWarehouse(), TimeWindow.getTimeWindow(8));
+        deliveries.add(0, warehouseDelivery);
+        Graph graph = new DeliveryGraph(this.deliveriesList, cityMap, shortestPathAlgorithm);
         TSP1 tsp1 = new TSP1();
 
         // Grouping the deliveries by TimeWindow
         Map<TimeWindow, List<Integer>> deliveriesByTimeWindow = new HashMap<>();
-        for (Delivery d : deliveriesList) {
+        for (Delivery d : deliveries) {
             deliveriesByTimeWindow
                     .computeIfAbsent(d.getTimeWindow(), k -> new ArrayList<>())
-                    .add(deliveriesList.indexOf(d));
+                    .add(deliveries.indexOf(d));
         }
         // Computing TSP solution
         tsp1.searchSolution(5000, graph, deliveriesByTimeWindow);
@@ -68,14 +68,23 @@ public class Tour {
         List<Delivery> newDeliveries = new ArrayList<>(solution.length-1);
         List<Path> newPaths = new ArrayList<>(solution.length-1);
 
-        newDeliveries.add(deliveriesList.get(0));
-        for (int i=1; i<solution.length; i++) {
-            newDeliveries.add(deliveriesList.get(i));
+        if(solution.length > 1) {
+            newDeliveries.add(deliveries.get(solution[1]));
+        }
+        for (int i=2; i<solution.length; i++) {
+            newDeliveries.add(this.deliveriesList.get(i));
             newPaths.add(graph.getShortestPath(solution[i-1], solution[i]));
         }
 
-        deliveriesList.setAll(newDeliveries);
-        pathList = newPaths;
+        this.deliveriesList.setAll(newDeliveries);
+        this.pathList = newPaths;
+
+        System.out.println(Arrays.stream(solution).toList());
+        System.out.println(deliveries);
+        System.out.println(deliveriesList);
+        System.out.println(pathList);
+
+
     }
 
     public Courier getCourier() {
