4febae2c8d48155ad7ac4b5ac8ec572396da6e81
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-13 10:19:22 +0100

    Ajout de tests sur les positions

---
+++ src/test/java/fr/insalyon/agile/PositionTest.java
@@ -0,0 +1,36 @@
+package fr.insalyon.agile;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import fr.insalyon.geometry.CoordinateTransformer;
+import fr.insalyon.geometry.GeoCoordinates;
+import fr.insalyon.geometry.Position;
+
+class PositionTest {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/pointSubstract.csv", numLinesToSkip = 1)
+	void testPositionSubstract(float x1, float y1, float x2, float y2, float xExpected, float yExpected) {
+		Position p1 = new Position(x1, y1);
+		Position p2 = new Position(x2, y2);
+		
+		p1.substract(p2);
+		
+		Position expectedPosition = new Position(xExpected, yExpected);
+		assertEquals(expectedPosition, p1);
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/pointDivide.csv", numLinesToSkip = 1)
+	void testPositionDivide(float x1, float y1, double scale, float xExpected, float yExpected) {
+		Position p1 = new Position(x1, y1);
+		
+		p1.divide(scale);
+		
+		Position expectedPosition = new Position(xExpected, yExpected);
+		assertEquals(expectedPosition, p1);
+	}
+}
