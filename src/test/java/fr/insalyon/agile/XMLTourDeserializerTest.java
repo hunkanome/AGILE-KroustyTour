a5b12881dc2bc64c0eaaac40dec8e73edfa04201
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-13 17:39:25 +0100

    Ajout de tests sur la serialization de tours

--- src/test/java/fr/insalyon/agile/XMLTourDeserializerTest.java
+++ src/test/java/fr/insalyon/agile/XMLTourDeserializerTest.java
@@ -7,15 +7,38 @@ import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import fr.insalyon.algorithm.AStar;
 import fr.insalyon.city_map_xml.BadlyFormedXMLException;
+import fr.insalyon.geometry.GeoCoordinates;
 import fr.insalyon.model.CityMap;
 import fr.insalyon.model.Courier;
+import fr.insalyon.model.Delivery;
+import fr.insalyon.model.Intersection;
+import fr.insalyon.model.Segment;
+import fr.insalyon.model.TimeWindow;
 import fr.insalyon.model.Tour;
 import fr.insalyon.seralization.XMLTourDeserializer;
 
 class XMLTourDeserializerTest {
+	
+	private static CityMap cityMap;
+	private static Intersection intersection;
+	private static Intersection intersection1;
+	
+	@BeforeAll
+	static void createCityMap() {
+		intersection = new Intersection(0, new GeoCoordinates(1f, 1f), 0);
+		intersection1 = new Intersection(1, new GeoCoordinates(1f, 2f), 1);
+		Segment segment = new Segment(intersection, intersection1, "test", 0);
+		intersection.addOutwardSegment(segment);
+		cityMap = new MockCityMap();
+		cityMap.setWarehouse(intersection);
+		cityMap.addIntersection(intersection);
+		cityMap.addIntersection(intersection1);
+	}
 
 	@Test
 	void testEmptyTourList() throws Exception {
@@ -87,6 +110,28 @@ class XMLTourDeserializerTest {
 		assertEquals(expected, actual);
 	}
 	
-	// TODO faire avec des tours remplis
+	@Test
+	void testOneTourWithOneDelivery() throws Exception {
+		XMLTourDeserializer deserializer = new XMLTourDeserializer();
+		deserializer.setCityMap(cityMap);
+		String inputXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><tours cityMapHash=\"1\"><tour courierId=\"1\"><delivery intersectionId=\"0\" timeWindow=\"8\"/></tour></tours>";
+		ByteArrayInputStream in = new ByteArrayInputStream(inputXml.getBytes());
+		deserializer.setInputFile(in);
+		deserializer.deserialize();
+
+		List<Tour> actual = deserializer.getTours();
+		List<Tour> expected = new ArrayList<>();
+		Tour tour = new Tour(new Courier(1));
+		Delivery delivery = new Delivery(intersection, TimeWindow.getTimeWindow(8));
+		tour.addDelivery(delivery, cityMap, new AStar());
+		expected.add(tour);
+		
+		assertEquals(expected.size(), actual.size());
+		
+		Tour expectedTour = expected.get(0);
+		Tour actualTour = actual.get(0);
+		assertEquals(expectedTour.getCourier().getId(), actualTour.getCourier().getId());
+		assertEquals(expectedTour.getDeliveriesList().get(0).getLocation(), actualTour.getDeliveriesList().get(0).getLocation());
+	}
 
 }
--- src/test/java/fr/insalyon/agile/XMLTourSerializerTest.java
+++ src/test/java/fr/insalyon/agile/XMLTourSerializerTest.java
@@ -6,14 +6,37 @@ import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import fr.insalyon.algorithm.AStar;
+import fr.insalyon.geometry.GeoCoordinates;
 import fr.insalyon.model.CityMap;
 import fr.insalyon.model.Courier;
+import fr.insalyon.model.Delivery;
+import fr.insalyon.model.Intersection;
+import fr.insalyon.model.Segment;
+import fr.insalyon.model.TimeWindow;
 import fr.insalyon.model.Tour;
 import fr.insalyon.seralization.XMLTourSerializer;
 
 class XMLTourSerializerTest {
+	
+	private static CityMap cityMap;
+	private static Intersection intersection;
+	private static Intersection intersection1;
+	
+	@BeforeAll
+	static void createCityMap() {
+		intersection = new Intersection(0, new GeoCoordinates(1f, 1f), 0);
+		intersection1 = new Intersection(1, new GeoCoordinates(1f, 2f), 1);
+		Segment segment = new Segment(intersection, intersection1, "test", 0);
+		intersection.addOutwardSegment(segment);
+		cityMap = new MockCityMap();
+		cityMap.setWarehouse(intersection);
+		cityMap.addIntersection(intersection);
+		cityMap.addIntersection(intersection1);
+	}
 
 	@Test
 	void testEmptyTourList() throws Exception {
@@ -28,10 +51,10 @@ class XMLTourSerializerTest {
 
 		String actual = out.toString().trim();
 		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><tours cityMapHash=\"1\"/>";
-		
+
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	void testEmptyTour() throws Exception {
 		XMLTourSerializer serializer = new XMLTourSerializer();
@@ -47,10 +70,76 @@ class XMLTourSerializerTest {
 
 		String actual = out.toString().trim();
 		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><tours cityMapHash=\"1\"><tour courierId=\"1\"/></tours>";
-		
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testOneTourWithOneDelivery() throws Exception {
+		XMLTourSerializer serializer = new XMLTourSerializer();
+		List<Tour> tours = new ArrayList<>();
+		Tour tour = new Tour(new Courier(1));
+		Delivery delivery = new Delivery(intersection, TimeWindow.getTimeWindow(8));
+		tour.addDelivery(delivery, cityMap, new AStar());
+		tours.add(tour);
+		serializer.setTours(tours);
+		serializer.setCityMap(cityMap);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		serializer.setFile(out);
+		serializer.serialize();
+
+		String actual = out.toString().trim();
+		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><tours cityMapHash=\"1\"><tour courierId=\"1\"><delivery intersectionId=\"0\" timeWindow=\"8\"/></tour></tours>";
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testOneTourWithManyDeliveries() throws Exception {
+		XMLTourSerializer serializer = new XMLTourSerializer();
+		List<Tour> tours = new ArrayList<>();
+		Tour tour = new Tour(new Courier(1));
+		Delivery delivery = new Delivery(intersection, TimeWindow.getTimeWindow(8));
+		Delivery delivery1 = new Delivery(intersection1, TimeWindow.getTimeWindow(10));
+		tour.addDelivery(delivery, cityMap, new AStar());
+		tour.addDelivery(delivery1, cityMap, new AStar());
+		tours.add(tour);
+		serializer.setTours(tours);
+		serializer.setCityMap(cityMap);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		serializer.setFile(out);
+		serializer.serialize();
+
+		String actual = out.toString().trim();
+		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><tours cityMapHash=\"1\"><tour courierId=\"1\"><delivery intersectionId=\"0\" timeWindow=\"8\"/><delivery intersectionId=\"1\" timeWindow=\"10\"/></tour></tours>";
+
 		assertEquals(expected, actual);
 	}
 	
-	// TODO : ajouter des tests avec des tours remplis
+	@Test
+	void testManyToursWithManyDeliveries() throws Exception {
+		XMLTourSerializer serializer = new XMLTourSerializer();
+		List<Tour> tours = new ArrayList<>();
+		Tour tour = new Tour(new Courier(1));
+		Delivery delivery = new Delivery(intersection, TimeWindow.getTimeWindow(8));
+		tour.addDelivery(delivery, cityMap, new AStar());
+		tours.add(tour);
+		
+		Tour tour1 = new Tour(new Courier(2));
+		Delivery delivery1 = new Delivery(intersection1, TimeWindow.getTimeWindow(10));
+		tour1.addDelivery(delivery1, cityMap, new AStar());
+		tours.add(tour1);
+		
+		serializer.setTours(tours);
+		serializer.setCityMap(cityMap);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		serializer.setFile(out);
+		serializer.serialize();
+
+		String actual = out.toString().trim();
+		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><tours cityMapHash=\"1\"><tour courierId=\"1\"><delivery intersectionId=\"0\" timeWindow=\"8\"/></tour><tour courierId=\"2\"><delivery intersectionId=\"1\" timeWindow=\"10\"/></tour></tours>";
+
+		assertEquals(expected, actual);
+	}
 
 }
