445a7253830c058f8ea66608c8a9939f59f0f3e0
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-13 10:19:12 +0100

    Ajout de tests sur la transformation de positions

--- src/test/java/fr/insalyon/agile/CoordinateTransformerTest.java
+++ src/test/java/fr/insalyon/agile/CoordinateTransformerTest.java
@@ -2,6 +2,8 @@ package fr.insalyon.agile;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import fr.insalyon.geometry.CoordinateTransformer;
 import fr.insalyon.geometry.GeoCoordinates;
@@ -15,9 +17,11 @@ class CoordinateTransformerTest {
 		GeoCoordinates sePoint = new GeoCoordinates(0f, 10f);
 		CoordinateTransformer transformer = new CoordinateTransformer(nwPoint, sePoint, 100f, 100f);
 		GeoCoordinates coord = new GeoCoordinates(5f, 5f);
-		Position position = transformer.transformToPosition(coord);
-		assertEquals(50f, position.getX().floatValue());
-		assertEquals(50f, position.getY().floatValue());
+		
+		Position actualPosition = transformer.transformToPosition(coord);
+		Position expectedPosition = new Position(50f, 50f);
+		
+		assertEquals(expectedPosition, actualPosition);
 	}
 
 	@Test
@@ -26,9 +30,11 @@ class CoordinateTransformerTest {
 		GeoCoordinates sePoint = new GeoCoordinates(0f, 10f);
 		CoordinateTransformer transformer = new CoordinateTransformer(nwPoint, sePoint, 100f, 100f);
 		GeoCoordinates coord = new GeoCoordinates(10f, 0f);
-		Position position = transformer.transformToPosition(coord);
-		assertEquals(0f, position.getX().floatValue());
-		assertEquals(0f, position.getY().floatValue());
+		
+		Position actualPosition = transformer.transformToPosition(coord);
+		Position expectedPosition = new Position(0f, 0f);
+		
+		assertEquals(expectedPosition, actualPosition);
 	}
 
 	@Test
@@ -37,9 +43,11 @@ class CoordinateTransformerTest {
 		GeoCoordinates sePoint = new GeoCoordinates(0f, 10f);
 		CoordinateTransformer transformer = new CoordinateTransformer(nwPoint, sePoint, 100f, 100f);
 		GeoCoordinates coord = new GeoCoordinates(0f, 5f);
-		Position position = transformer.transformToPosition(coord);
-		assertEquals(50f, position.getX().floatValue());
-		assertEquals(100f, position.getY().floatValue());
+		
+		Position actualPosition = transformer.transformToPosition(coord);
+		Position expectedPosition = new Position(50f, 100f);
+		
+		assertEquals(expectedPosition, actualPosition);
 	}
 
 	@Test
@@ -54,6 +62,36 @@ class CoordinateTransformerTest {
 		String actualMessage = e.getMessage();
 
 		assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	void testTransformPointWithScale() {
+		GeoCoordinates nwPoint = new GeoCoordinates(10f, 0f);
+		GeoCoordinates sePoint = new GeoCoordinates(0f, 10f);
+		CoordinateTransformer transformer = new CoordinateTransformer(nwPoint, sePoint, 100f, 100f);
+		Position originPosition = new Position(5f, 5f);
+		
+		Position translation = new Position(0f, 0f);
+		double scale = 1f;
+		Position actualPosition = transformer.transformToDragAndZoom(originPosition, translation, scale);
+		Position expectedPosition = new Position(5f, 5f);
+		
+		assertEquals(expectedPosition, actualPosition);
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/pointTransformation.csv", numLinesToSkip = 1)
+	void testTransformPointWithScaleAndTranslation(float x, float y, double scale, float xTranslation, float yTranslation, float xExpected, float yExpected) {
+		GeoCoordinates nwPoint = new GeoCoordinates(10f, 0f);
+		GeoCoordinates sePoint = new GeoCoordinates(0f, 10f);
+		CoordinateTransformer transformer = new CoordinateTransformer(nwPoint, sePoint, 100f, 100f);
+		Position originPosition = new Position(x, y);
+		
+		Position translation = new Position(xTranslation, yTranslation);
+		Position actualPosition = transformer.transformToDragAndZoomPosition(originPosition, translation, scale);
+		Position expectedPosition = new Position(xExpected, yExpected);
+		
+		assertEquals(expectedPosition, actualPosition);
 	}
 	
 }
