037d27a880daed515d34642adb4fe7d5789a7335
arosard <alexandre.rosard@gmail.com>
2023-12-14 17:57:47 +0100

    Separate Equals' test in SegmentTest into multiple smaller tests

--- src/test/java/fr/insalyon/agile/SegmentTest.java
+++ src/test/java/fr/insalyon/agile/SegmentTest.java
@@ -10,34 +10,80 @@ import static org.junit.jupiter.api.Assertions.*;
 class SegmentTest {
 
     @Test
-    void testSegmentEquals() {
+    void testEqualsItself() {
+        Intersection intersection1 = new Intersection(1L, new GeoCoordinates(0f, 0f), 15);
+        Intersection intersection2 = new Intersection(2L, new GeoCoordinates(0f, 0f), 16);
+        String street1 = "Rue un";
+        float length1 = 10f;
+
+        Segment segment1 = new Segment(intersection1, intersection2, street1, length1);
+
+        assertEquals(segment1, segment1);
+    }
+
+    @Test
+    void testEqualsNull() {
+        Intersection intersection1 = new Intersection(1L, new GeoCoordinates(0f, 0f), 15);
+        Intersection intersection2 = new Intersection(2L, new GeoCoordinates(0f, 0f), 16);
+        String street1 = "Rue un";
+        float length1 = 10f;
+
+        Segment segment1 = new Segment(intersection1, intersection2, street1, length1);
+
+        assertNotEquals(segment1, null);
+    }
+
+    @Test
+    void testEqualsDifferentClass() {
+        Intersection intersection1 = new Intersection(1L, new GeoCoordinates(0f, 0f), 15);
+        Intersection intersection2 = new Intersection(2L, new GeoCoordinates(0f, 0f), 16);
+        String street1 = "Rue un";
+        float length1 = 10f;
+
+        Segment segment1 = new Segment(intersection1, intersection2, street1, length1);
+
+        assertNotEquals(segment1, new Object());
+    }
+
+    @Test
+    void testEqualsDifferentDestination() {
+        Intersection intersection1 = new Intersection(1L, new GeoCoordinates(0f, 0f), 15);
+        Intersection intersection2 = new Intersection(2L, new GeoCoordinates(0f, 0f), 16);
+        Intersection intersection3 = new Intersection(3L, new GeoCoordinates(0f, 0f), 17);
+        String street1 = "Rue un";
+        float length1 = 10f;
+
+        Segment segment1 = new Segment(intersection1, intersection2, street1, length1);
+        Segment segment2 = new Segment(intersection1, intersection3, street1, length1);
+
+        assertNotEquals(segment1, segment2);
+    }
+
+    @Test
+    void testEqualsDifferentName() {
         Intersection intersection1 = new Intersection(1L, new GeoCoordinates(0f, 0f), 15);
         Intersection intersection2 = new Intersection(2L, new GeoCoordinates(0f, 0f), 16);
         String street1 = "Rue un";
         String street2 = "Rue deux";
         float length1 = 10f;
+
+        Segment segment1 = new Segment(intersection1, intersection2, street1, length1);
+        Segment segment2 = new Segment(intersection1, intersection2, street2, length1);
+
+        assertNotEquals(segment1, segment2);
+    }
+
+    @Test
+    void testEqualsDifferentLength() {
+        Intersection intersection1 = new Intersection(1L, new GeoCoordinates(0f, 0f), 15);
+        Intersection intersection2 = new Intersection(2L, new GeoCoordinates(0f, 0f), 16);
+        String street1 = "Rue un";
+        float length1 = 10f;
         float length2 = 20f;
 
         Segment segment1 = new Segment(intersection1, intersection2, street1, length1);
-        Segment segment2 = new Segment(intersection1, intersection2, street1, length1);
+        Segment segment2 = new Segment(intersection1, intersection2, street1, length2);
 
-        assertEquals(segment1, segment1);
-        assertNotEquals(segment1, null);
-        assertNotEquals(segment1, new Object());
-        assertEquals(segment1, segment2);
-
-        // If origin is different we ignore it to avoid infinite recursion with Intersection
-
-        // If destination is different
-        segment2 = new Segment(intersection1, intersection1, street1, length1);
-        assertNotEquals(segment1, segment2);
-
-        // If name is different
-        segment2 = new Segment(intersection1, intersection2, street2, length1);
-        assertNotEquals(segment1, segment2);
-
-        // If length is different
-        segment2 = new Segment(intersection1, intersection2, street1, length2);
         assertNotEquals(segment1, segment2);
     }
 }
