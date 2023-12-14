746c240b8d219f041bb7f724ebf2ae46d8a35a24
Anto <antonin.sibileau@gmail.com>
2023-12-14 11:06:02 +0100

    Adding Position tests

--- src/test/java/fr/insalyon/agile/PositionTest.java
+++ src/test/java/fr/insalyon/agile/PositionTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import fr.insalyon.geometry.CoordinateTransformer;
 import fr.insalyon.geometry.GeoCoordinates;
 import fr.insalyon.geometry.Position;
 
@@ -32,5 +31,41 @@ class PositionTest {
 		
 		Position expectedPosition = new Position(xExpected, yExpected);
 		assertEquals(expectedPosition, p1);
+	}
+	@Test
+	public void testEquals() {
+		Position position1 = new Position(1.0f, 2.0f);
+		Position position2 = new Position(1.0f, 2.0f);
+        assertEquals(position1, position1);
+        assertEquals(position1, position2);
+	}
+	@Test
+	public void testNotEquals() {
+		Position position1 = new Position(1.0f, 3.0f);
+		Position position2 = new Position(1.0f, 2.0f);
+        assertNotEquals(position1, position2);
+        assertNotEquals(position1, null);
+		assertNotEquals(position1, new GeoCoordinates(1.0f, 2.0f));
+	}
+
+	@Test
+	public void toStringTest() {
+		Position position = new Position(1.0f, 2.0f);
+		assertEquals("Position [x=1.0, y=2.0]", position.toString());
+	}
+
+	//test for distanceTo
+	@Test
+	public void testDistanceTo() {
+		Position position1 = new Position(1.0f, 2.0f);
+		Position position2 = new Position(1.0f, 2.0f);
+		Float expected = (float) 0;
+		assertEquals(position1.distanceTo(position2), expected);
+		position2 = new Position(4.0f, 2.0f);
+		expected = (float) 3;
+		assertEquals(position1.distanceTo(position2), expected);
+		position2 = new Position(1.0f, 4.0f);
+		expected = (float) 2;
+		assertEquals(position1.distanceTo(position2), expected);
 	}
 }
