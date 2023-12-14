9a605f3d6356c13ce65ab357754090bf3f1e799c
Martin Bonnefoy <martin.bonnefoy@gmail.com>
2023-12-14 10:40:07 +0100

    Fix des tests de serializer/deserializer

--- src/test/java/fr/insalyon/agile/PositionTest.java
+++ src/test/java/fr/insalyon/agile/PositionTest.java
@@ -1,12 +1,10 @@
 package fr.insalyon.agile;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import fr.insalyon.geometry.CoordinateTransformer;
-import fr.insalyon.geometry.GeoCoordinates;
 import fr.insalyon.geometry.Position;
 
 class PositionTest {
